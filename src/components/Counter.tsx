91b7fb837236fd7736f45cd5d344020868f55d54
milmilmilkim <sound@plea.kr>
2023-03-30 15:17:34 +0900

    twin.marco 추가

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,6 @@
 import { useState, useCallback } from 'react';
+import tw, { TwStyle } from 'twin.macro';
+import styled from 'styled-components';
 
 const Counter = () => {
   const [number, setNumber] = useState(0);
@@ -16,14 +18,20 @@ const Counter = () => {
   }, []);
 
   return (
-    <>
+    <StyledCounter>
       <h1>테스트 컴포넌트</h1>
       <br />
       {number}
+      <hr />
       <button onClick={() => handlePlusMinus(plus)}>+</button>
       <button onClick={() => handlePlusMinus(minus)}>-</button>
-    </>
+    </StyledCounter>
   );
 };
+
+const StyledCounter = styled.div`
+  ${tw`rounded-lg shadow-lg`}
+  width: 300px;
+`;
 
 export default Counter;
