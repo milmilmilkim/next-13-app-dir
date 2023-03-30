f13b8cbcdc0e5e228875807605dbcaeb8ef759ee
milmilmilkim <sound@plea.kr>
2023-03-30 15:19:00 +0900

    use client 추가

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import { useState, useCallback } from 'react';
-import tw, { TwStyle } from 'twin.macro';
+import tw from 'twin.macro';
 import styled from 'styled-components';
 
 const Counter = () => {
